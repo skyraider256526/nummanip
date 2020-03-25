3178fbdc7e66d095eb1adcc8204a3ba58785adef
skyraider <akashyadav@gmail.com>
2020-03-25 12:15:32 +0530

    new release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,6 +1,10 @@
 package calc
 
-//returns sum of two integers
-func Add(i int, j int ) int{
-	return i + j
+// Add returns sum of two integers
+func Add(numbers ...int) int {
+	sum := 0
+	for _, num := range numbers {
+		sum += num
+	}
+	return sum
 }
